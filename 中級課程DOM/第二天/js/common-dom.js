e46908b53161c7e683b48ee0edfd43516cd96078
waha1206 <waha1206@gmail.com>
2020-07-23 02:40:28 +0800

    第二天完成

--- 中級課程DOM/第二天/js/common-dom.js
+++ 中級課程DOM/第二天/js/common-dom.js
@@ -16,6 +16,29 @@ function getTowNumber(num) {
 }
 
 /**
+ * ie8不支持 textContent，火狐跟谷哥支持
+ * set 需要傳進去兩個參數 元素對象 --- object 與 text --- 值
+ */
+function setinnerText(element, text) {
+    if (typeof element.textContent) {
+        element.innerText = text;
+    } else {
+        element.textContent = text;
+    }
+}
+/**
+ * get text 只需要傳進去 元素對象即可取得 innerText 或是 textContent
+ *
+ */
+function getinnerText(element) {
+    if (typeof element.textContent) {
+        return element.textContent;
+    } else {
+        return element.textContent;
+    }
+}
+
+/**
  *
  *
  * @param {*} dt 傳入一個函數 new Date()
